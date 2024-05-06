6e8f194ea21232ac6aa292cbe5bb79f3bef8999f
tterrazzz <alexandre.g.van@gmail.com>
2024-05-06 03:51:54 -0500

    redis object cache connected to wp and working

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -66,21 +66,17 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	
 	#ln -s /var/www/html/wp-content/plugins/redis-cache/includes/object-cache.php /var/www/html/wp-content/object-cache.php
 
-	#/usr/local/bin/wp config set --allow-root WP_REDIS_HOST $REDIS_HOST --raw
-	#/usr/local/bin/wp config set --allow-root WP_REDIS_PORT $REDIS_PORT --raw
-	#/usr/local/bin/wp config set --allow-root WP_CACHE_KEY_SALT $DB_NAME --raw
-	#/usr/local/bin/wp config set --allow-root WP_REDIS_CLIENT 'phpredis' --raw
-#		WP_REDIS_PASSWORD $REDIS_PWD \
-	
 	sed -i "62i define ('WP_REDIS_HOST', '$REDIS_HOST');" /var/www/html/wp-config.php
 	sed -i "63i define ('WP_REDIS_PORT', '$REDIS_PORT');" /var/www/html/wp-config.php
 	sed -i "64i define ('WP_CACHE', true);" /var/www/html/wp-config.php
 	sed -i "65i define ('WP_REDIS_CLIENT', 'phpredis');" /var/www/html/wp-config.php
 	sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" /var/www/html/wp-config.php
+	sed -i "66i define ('WP_DEBUG_LOG', true);" /var/www/html/wp-config.php
+	sed -i "67i define ('WP_DEBUG_DISPLAY', false);" /var/www/html/wp-config.php
 
 	wp plugin install redis-cache --allow-root --path='/var/www/html/' --activate
 	wp plugin update --all --allow-root --path='/var/www/html/'
-	#wp redis enable --allow-root
+	wp redis enable --allow-root
 
 	chown -R nginx:nginx /var/www/html/*
 	find /var/www/html/ -type d -exec chmod 755 {} \;
