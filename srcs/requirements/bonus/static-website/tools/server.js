0974ed2ccbf11ce34b15a9381299413021170984
tterrazzz <alexandre.g.van@gmail.com>
2024-05-23 10:43:52 -0400

    port de static-website homogeneise, still not working

--- srcs/requirements/bonus/static-website/tools/server.js
+++ srcs/requirements/bonus/static-website/tools/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = '0.0.0.0';
-const port = 443;
+const port = 8082;
 
 const options = {
 	key: fs.readFileSync(path.join(__dirname, 'certs', 'avan-selfsigned.key')),
@@ -16,12 +16,12 @@ https.createServer(options, (req, res) => {
 	fs.readFile(filePath, (error, content) => {
 		if (error) {
 			res.writeHead(500);
-			res.end('Error: ${error.code} Bad Server Response\n');
+			res.end(`Error: ${error.code} Bad Server Response\n`);
 		} else {
 			res.writeHead(200, { 'Content-Type': 'test/html'});
 			res.end(content, 'utf-8');
 		}
 	});
 }).listen(port, hostname, () => {
-	console.log('Server running at https://${hostname}:${port}/');
+	console.log(`Server running at https://${hostname}:${port}/`);
 });
